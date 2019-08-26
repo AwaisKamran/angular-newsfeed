21c24da47a0aba309f3c99792d6a58143bd33de5
awaiskamranvroozi <awais.kamran@vroozi.com>
2019-08-27 01:21:29 +0500

    angular-newsfeed: feat(Project base) register component added.

--- angular-newsfeed/src/app/register/register.component.ts
+++ angular-newsfeed/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -7,16 +8,19 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  private data: any = { };q
+  private data: any = { };
   private registerForm: boolean = true;
   private error: boolean = false;
   private errorMessage: string = "Error! Please fill in all the fields.";
   private success: boolean = false;
   private successMessage: string = "Congratulations! you have registered successfully.";
   private selectedFile: File = null;
+  private imageUrl: any = "../../assets/profile_placeholder.jpg";
   @ViewChild('uploader', { static: false }) fileUploader;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   toggleForm(){
     this.registerForm = ! this.registerForm;
@@ -24,28 +28,34 @@ export class RegisterComponent {
 
   login(){}
 
-  register(){}
+  register(){
+    const fd = new FormData();
+    fd.append('image', this.selectedFile);
+    fd.append('name', "Bruce Wayne");
+    fd.append('email', "bruce@django.com");
+    fd.append('password', "django");
+    fd.append('ipAddress', "172.0.0.1");
+
+    console.log(fd);
+
+    this.http.post('http://localhost:56003/api/Users', fd)
+      .subscribe((res: any) => {
+        console.log(res.data);
+      }, (err: any) => {});
+  }
 
   public onFileSelected(event) {
     this.selectedFile = <File>event.target.files[0];
-    if (this.selectedFile) {
-      return this.upload();
-    }
+    
+    var container = this;
+    var reader = new FileReader();
+    reader.onload = function(){
+      container.imageUrl = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
   }
 
   uploadImage(){
     this.fileUploader.nativeElement.click();
   }
-
-  upload() {
-    /*const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
-      this.http.post('http://example.com/upload/image', fd)
-      .subscribe((res: any) => {
-        this.image = res.data;
-      }, (err: any) => {
-          // Show error message or make something.
-    });*/
-  }
-
 }
